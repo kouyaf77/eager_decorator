10394dc40e76916c8929d4bcb5fc344e3788264b
KouyaFukuda <kouyaf77@gmail.com>
2015-01-02 17:42:53 +0000

    Added class

--- lib/eager_decorator.rb
+++ lib/eager_decorator.rb
@@ -2,16 +2,18 @@ require "eager_decorator/version"
 require "singleton"
 
 module EagerDecorator
-  include Singleton
+  class Decorator
+    include Singleton
 
-  PATH = "#{Rails.root}/app/decorators/**/*.rb"
+    PATH = "#{Rails.root}/app/decorators/**/*.rb"
 
-  def initialize
-    Dir.glob(PATH).each do |file|
-      class_name= File.basename(file, ".rb").sub(/_decorator/, "")
-      constant = class_name.classify.constantize rescue nil
-      unless constant.nil?
-        constant.send :include, "#{class_name.classify}Decorator".constantize
+    def initialize
+      Dir.glob(PATH).each do |file|
+        class_name= File.basename(file, ".rb").sub(/_decorator/, "")
+        constant = class_name.classify.constantize rescue nil
+        unless constant.nil?
+          constant.send :include, "#{class_name.classify}Decorator".constantize
+        end
       end
     end
   end
